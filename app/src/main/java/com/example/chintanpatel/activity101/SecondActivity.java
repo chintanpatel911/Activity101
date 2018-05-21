fbe90abd245448b8d3c80c6fcaa40a4de8b54309
Chintan Patel <chintanpatel911@gmail.com>
2018-05-21 09:30:30 -0400

    Initial commit

---
+++ app/src/main/java/com/example/chintanpatel/activity101/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.chintanpatel.activity101;
+
+import android.os.Build;
+import android.app.Activity;
+
+/**
+ * Created by Chintan Patel on 1/31/2018.
+ */
+
+public class SecondActivity {
+    public void onCreate(Build savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+}
